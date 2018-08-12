e753ad27b5164a0e6226e96d4e63669a18276fcc
Katie <katie@imagings-MacBook-Pro.local>
2018-08-11 18:44:02 -0700

    final files

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# bamazon
+# bamazon week 12 homework assignment
+
+This homework assignment was to create a bamazon store using nodejs and a sql database to hold the products.
+
+The user can open the store front and see all the items for sale - a unique item Id number and the price are also listed.
+The user is then prompted to select an item Id number for the purchase they would like to make and once that is entered, the user will be prompted to choose a quantity of the selected items to purchase.
+
+If the stock is available the total will be returned and the user will be asked if they would like to continue shopping.
+
+If the user responds yes, they will be prompted to select another Item Id and a quantity. The amount of the purchase will be added to the previous order and the user can keep shopping or stop at any time. When the user chooses to stop shopping the application will return the total owed for all the purchases made and log off. The sql database will also (upon update) reflect the user's purchases by removing the items from stock.
+
+If the user selects an item and a quantity that does not have available stock, the application will respond that there is not that many items available for purchase, but also give the user the opportunity to try again.
+
+
+*** npm packages used:
+inquirer
+mysql
+cli-table
+colors
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,9 +2,11 @@ var inquirer = require('inquirer');
 var mysql = require('mysql');
 var Table = require('cli-table');
 var colors = require('colors');
+var totalPrice = 0;
 
-
-console.log(colors.green('hello'));
+console.log(colors.bold.green("\n \n- - - - - - - - - - - - - - - - - - - - - - - "));
+console.log(colors.bold.green('Hello Welcome to BamaZon!'));
+console.log(colors.bold.green("- - - - - - - - - - - - - - - - - - - - - - -\n"));
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -22,33 +24,130 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    // connection.query(selectQuery + " " + where, function(err, response) {
-    //     if(err)throw err;
-    //     console.log(response);
+ 
+
 });
 
-function showDB() {
+var startUp = function () {
     var table = new Table({
         head: ['Item ID', 'Product Name', 'Price'],
         colWidths: [10, 75, 10]
     });
 
     var query = "SELECT item_id, product_name, price FROM products";
-    
-    connection.query(query, function (err, res) {
-                for (var i = 0; i < res.length; i++) {
-                    table.push(
-                        [res[i].item_id,
-                        res[i].product_name,
-                        "$" + res[i].price]
-                    );
-                }
-                    console.log(table.toString().bold.red);
-                    connection.end();
-            });
-        }
- 
-showDB();
 
+    connection.query(query, function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].item_id,
+                    res[i].product_name,
+                    "$" + res[i].price
+                ]
+            );
+        }
+        console.log(table.toString().bold.red);
+        promptUser();
+        //table is loaded and ready
+    });
+    
+
+    function promptUser() {
+    
+        inquirer.prompt([
+            {
+            type: 'input',
+            name: 'item',
+            message: '\nPlease select an item to buy, use the Item ID number',
+            validate: function integerCheck(item) {
+                if (isNaN(item) === false) {
+                    return true;
+                } else {
+                    return false;
+                }
+                if(item !== res[0].item_id) {
+                    console.log("That item number does not exist");
+                }
+    
+    
+            }
+        },
+    
+            {
+                type: 'input',
+                name: 'quantity',
+                message: '\nHow many would you like?',
+                validate: function integerCheck(quantity) {
+                    if (isNaN(quantity) === false) {
+                        return true;
+    
+                    } else {
+                        return false;
+                    }
+    
+                }
+            }
+        ]).then(function (answer) {
+            
+            var select = "SELECT ?? FROM ?? WHERE ?? = ?";
+            var response = ['*', 'products', 'item_id', answer.item];
+            select = mysql.format(select, response);
+            connection.query(select, function (err, results) {
+                var totalPerSelection = answer.quantity * results[0].price;
+                totalPrice += totalPerSelection;
+                if (answer.quantity <= results[0].stock_quantity) {
+                    console.log("Thank you for your order of: \n" + answer.quantity + " " + results[0].product_name +  "\nTotal comes to: $" + totalPrice.toFixed(2));
+    
+                    var quantitySelected = results[0].stock_quantity - answer.quantity;
+                    var sQuery = connection.query(
+                        "UPDATE products SET ? WHERE ?", [{
+                                stock_quantity: quantitySelected
+                            },
+                            {
+                                item_id: results[0].item_id
+                            }
+                        ],
+                        function (err, results) {
+                            //console.log("hello we made it this far");
+                            var yesNo = function(){
+                                inquirer.prompt([
+                                    {
+                                        type: 'confirm',
+                                        message: 'Do you want to keep shopping?',
+                                        name: 'confirm',
+                                        default: true
+                        
+                                    }
+                                   
+                                ]).then(function(response){
+                                    
+                                    if(response.confirm){
+                                        promptUser();
+                                    } else if(totalPerSelection !==0){
+                                        
+                                        console.log("Total owed for today's purchses: \n$" + totalPrice.toFixed(2) + "\nSorry to see you go - come back soon!\n\n\n");
+                                        console.log(colors.bold.green("- - - - - - - - - - - - - - - - - - - - - - -\n"));
+
+                                        connection.end();            
+                        
+                                    }
+                                   
+                                })
+                            }
+                            yesNo();
+                            
+                        }
+                    )
+                } else {
+                    totalPrice = 0;
+                    console.log("I'm sorry, but we currently only have " + results[0].stock_quantity + " in stock. \nWould you like to try again?");
+                    promptUser();
+                }
+            })
+    
+        })
+    }
+}
+
+
+startUp();
 
