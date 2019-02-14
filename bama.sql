f070c6ea5c6c5c198ae4454a29e4863e9dbb2489
koneone2002 <katie@imagings-MacBook-Pro.local>
2019-02-14 15:01:36 -0800

    fixing vulns

--- bama.sql
+++ bama.sql
@@ -1,31 +1,21 @@
-DROP DATABASE IF EXISTS bamazonDB;
 
-CREATE database bamazonDB;
+DROP DATABASE if EXISTS seingeld_db;
 
-USE bamazonDB;
+CREATE DATABASE seinfeld_db;
 
-CREATE TABLE products (
-    item_id INT NOT NULL AUTO_INCREMENT,
-    product_name VARCHAR(255) NOT NULL,
-    department_name VARCHAR(255) NOT NULL,
-    price FLOAT NOT NULL,
-    stock_quantity INT NOT NULL,
-    PRIMARY KEY (item_id)
+USE seinfeld_db;
 
+CREATE TABLE actors (
+	id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    coolness_points INT NOT NULL,
+    attitude VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Haribo Gold-Bears Gummy Candy Goldbaren', 'Candy', 29.44, 59);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Kuhn Rikon Knife Stand', 'Housewares', 40.00, 14);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('GoWISE USA Programmable Air Fryer', 'Housewares', 69.00, 78);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('CubeTech Health Light', 'Lighting', 22.29, 2);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Jokari Beverage Opener', 'Housewares', 7.00, 20);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Inf-way Garden Genie Gloves', 'Garden', 8.99, 17);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('The Sushi Bazooka', 'Housewares', 5.00, 16);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Moosh Natural Dog Shampoo', 'Petcare', 15.99, 32);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Unicorn Tape Dispenser', 'Housewares', 10.00, 25);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Running Buddy Pouch', 'Fitness', 23.99, 10);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Dash Mini Waffle Maker', 'Housewares', 10.39, 23);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Zotz Strings, Blue Razz, Orange and Grape', 'Candy', 24.99, 48);
-
+INSERT INTO actors  VALUES ("Jerry", 30, "sarcastic");
+INSERT INTO actors  VALUES ("Elaine", 50, "funny");
+INSERT INTO actors  VALUES ("George", 2, "annoying");
+INSERT INTO actors  VALUES ("Kramer", 100, "crazy");
 
 
